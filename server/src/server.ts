a41c2dc89fff74c7e9108d95735ed1019c116175
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2023-04-16 22:32:25 -0300

    create database connection
    -SQlite
    -criação das tabelas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
